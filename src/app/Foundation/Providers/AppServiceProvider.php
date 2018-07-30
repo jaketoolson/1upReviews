ffcc9aa0be081c397e68be7bf6cb478b181e3b80
Jake Toolson <jake.toolson@fire-engine-red.com>
2018-07-30 16:18:09 -0700

    enable query logging by env

--- src/app/Foundation/Providers/AppServiceProvider.php
+++ src/app/Foundation/Providers/AppServiceProvider.php
@@ -20,33 +20,34 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        DB::listen(function($query) {
-
-            // Iterate over all the bindings and parse them to correct string values in order to generate
-            // a complete raw sql query.
-            foreach ($query->bindings as $i => $binding) {
-                if ($binding instanceof DateTime) {
-                    $query->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
-                } else {
-                    if (is_string($binding)) {
-                        $query->bindings[$i] = "'$binding'";
+        if (env('LOG_QUERIES', false) === true) {
+            DB::listen(function($query) {
+                // Iterate over all the bindings and parse them to correct string values in order to generate
+                // a complete raw sql query.
+                foreach ($query->bindings as $i => $binding) {
+                    if ($binding instanceof DateTime) {
+                        $query->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                    } else {
+                        if (is_string($binding)) {
+                            $query->bindings[$i] = "'$binding'";
+                        }
                     }
                 }
-            }
 
-            $sqlQuery = str_replace(array('%', '?'), array('%%', '%s'), $query->sql);
-            $sqlQuery = vsprintf($sqlQuery, $query->bindings);
+                $sqlQuery = str_replace(array('%', '?'), array('%%', '%s'), $query->sql);
+                $sqlQuery = vsprintf($sqlQuery, $query->bindings);
 
-            $dbLog = new Logger('query');
-            $dbLog->pushHandler(
-                new RotatingFileHandler(
-                    storage_path('logs/queries.log'),
-                    5,
-                    Logger::DEBUG
-                )
-            );
-            $dbLog->info($sqlQuery, ['Bindings' => $query->bindings, 'Time' => $query->time]);
-        });
+                $dbLog = new Logger('query');
+                $dbLog->pushHandler(
+                    new RotatingFileHandler(
+                        storage_path('logs/queries.log'),
+                        5,
+                        Logger::DEBUG
+                    )
+                );
+                $dbLog->info($sqlQuery, ['Bindings' => $query->bindings, 'Time' => $query->time]);
+            });
+        }
     }
 
     /**
